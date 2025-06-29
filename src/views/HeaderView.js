4d55d832bd0e4c5344914e178a89e899fd938ffa
Aijr07 <ahmadsyahid397@gmail.com>
2025-06-29 12:11:33 +0800

    tess

--- src/views/HeaderView.js
+++ src/views/HeaderView.js
@@ -39,7 +39,7 @@ export class HeaderView {
                 </a>
                 <a href="#/add" class="hover:bg-green-700 p-2 rounded-md flex items-center space-x-2 transition duration-200 ease-in-out">
                   <svg class="w-5 h-5" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
-                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 9v3m0 0v3m0-3h3m-3 0H9m12A9 9 0 113 12a9 9 0 0118 0z"></path>
+                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 9v3m0 0v3m0-3h3m-3 0H9m12a9 9 0 11-18 0 9 9 0 0118 0z"></path>
                   </svg>
                   <span>Tambah Kisah Baru</span>
                 </a>
